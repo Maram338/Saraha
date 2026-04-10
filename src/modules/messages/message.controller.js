12b29ad139acf1ee4b9dd1abba6a23438ec5f1dd
Maram338 <Maramhaiba@gmail.com>
2026-04-10 04:53:52 +0200

    message multer updated

--- src/modules/messages/message.controller.js
+++ src/modules/messages/message.controller.js
@@ -1,17 +1,14 @@
 import { Router } from "express"
 import * as MS from "./message.service.js"
 import * as MV from "./message.validation.js"
-import { multer_host, multer_local } from "../../common/middleware/multer.js"
+import { multer_host} from "../../common/middleware/multer.js"
 import { validation } from "../../common/middleware/validation.js"
 import { multerEnum } from "../../common/enum/multer.enum.js"
 import { authentication } from "../../common/middleware/authentication.js"
 const messageRouter = Router({caseSensitive:true, strict:true, mergeParams:true})
 
 messageRouter.post("/send",
-    multer_host({
-        customPath: "messages",
-        customType: multerEnum.image
-    }).array("attachments", 3),
+    multer_host(multerEnum.image).array("attachments", 3),
     validation(MV.sendMessageSchema),
     MS.sendMessage
 )
